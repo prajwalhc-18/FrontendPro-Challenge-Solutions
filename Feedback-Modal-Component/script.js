34692973bb2adc0ea903c10b77f6e3cc46d75f38
prajwalhc-18 <prajwalhc1806@gmail.com>
2024-04-05 23:57:04 +0530

    dynamicness

--- Feedback-Modal-Component/script.js
+++ Feedback-Modal-Component/script.js
@@ -1,3 +1,26 @@
-document.getElementById("showFeedback").addEventListener("click", function() {
-  window.location.href = "index.html";
-});
+function rate(rating) {
+  // Highlight selected rating
+  const ratings = document.querySelectorAll('.rating div');
+  ratings.forEach((div, index) => {
+    if (index < rating) {
+      div.classList.add('selected');
+    } else {
+      div.classList.remove('selected');
+    }
+  });
+}
+
+function cancel() {
+  // Reset rating
+  const ratings = document.querySelectorAll('.rating div');
+  ratings.forEach(div => {
+    div.classList.remove('selected');
+  });
+}
+
+function submitRating() {
+  // Get selected rating
+  const selectedRating = document.querySelector('.rating div.selected a').innerText;
+  alert(`You rated FrontendPro ${selectedRating}/10. Thank you for your feedback!`);
+  // You can add further logic here to submit the rating to a server or perform other actions
+}
